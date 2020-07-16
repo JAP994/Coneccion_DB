7a1a7c5d85a4034d624c884f7d820a0c0000ca7d
Jsanchez <52222736+JAP994@users.noreply.github.com>
2020-07-15 23:01:12 -0500

    Transaccion Finalizada

---
+++ Coneccion_DB/ConexionPgsql.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Coneccion_DB
+{
+    class ConexionPgsql
+    {
+        NpgsqlConnection conn = new NpgsqlConnection("Server = localhost; User Id = postgres; Password = suco1994; Database = postgres;");
+
+        public DataTable Consultar()
+        {
+            string query = "SELECT * FROM cuentas";
+            NpgsqlCommand conector = new NpgsqlCommand(query, conn);
+            NpgsqlDataAdapter datos = new NpgsqlDataAdapter(conector);
+            DataTable tabla = new DataTable();
+            datos.Fill(tabla);
+            return tabla;
+        }
+
+        public void Tranferir(string textBox1, string textBox2, int textBox3)
+        {
+            try
+            {
+                conn.Open();
+                string query1 = "BEGIN";
+                NpgsqlCommand ejecutor1 = new NpgsqlCommand(query1, conn);
+                ejecutor1.ExecuteNonQuery();
+              
+                string query2 = $"UPDATE cuentas SET saldo = saldo - { textBox3 } WHERE nro_cuenta = '{ textBox1 }' and saldo >0 and saldo >= { textBox3 }";
+                NpgsqlCommand ejecutor2 = new NpgsqlCommand(query2, conn);
+                var e= ejecutor2.ExecuteNonQuery();
+                //MessageBox.Show(e+"");
+
+                string query3 = $"UPDATE cuentas SET saldo = saldo + { textBox3 } WHERE nro_cuenta = '{ textBox2 }'";
+                NpgsqlCommand ejecutor3 = new NpgsqlCommand(query3, conn);
+                var f=ejecutor3.ExecuteNonQuery();
+                //MessageBox.Show(f + "");
+                if (f == 1 && e == 1)
+                {
+                    //MessageBox.Show(e + "e" + f + "f");
+                    string query4 = "COMMIT";
+                    NpgsqlCommand ejecutor4 = new NpgsqlCommand(query4, conn);
+                    ejecutor4.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Transacción realizada con éxito.");
+                }
+                else
+                {
+                   // MessageBox.Show(e + "e" + f + "f");
+                    string query4 = "ROLLBACK";
+                    NpgsqlCommand ejecutor4 = new NpgsqlCommand(query4, conn);
+                    ejecutor4.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("La transacción no pudo ser realizada.");
+                }
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                MessageBox.Show("La transacción no pudo ser realizada.");
+                throw;
+            }
+           
+        }
+    }
+}
---
+++ Coneccion_DB/Form1.cs
@@ -0,0 +1,42 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coneccion_DB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ConexionPgsql conectandose = new ConexionPgsql();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = conectandose.Consultar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conectandose.Tranferir(textBox1.Text, textBox2.Text, Convert.ToInt32(textBox3.Text));
+                dataGridView1.DataSource = conectandose.Consultar();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Debe ingresar los datos correctamente.");
+            }
+        }
+    }
+}
